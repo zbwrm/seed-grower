ab525a5056cd15dd5e96651005912e8bd998abe7
Aydin O'Leary <aoleary@olin.edu>
2021-12-29 18:14:03 -0600

    Add more architectural features

--- generator_script.py
+++ generator_script.py
@@ -12,8 +12,11 @@ city.new_quality('biomes', 'geography', random.choice([1, 1, 1, 1, 2]))
 
 city.new_quality('building styles', 'architecture', random.randint(2,4))
 city.new_quality('building materials', 'architecture', random.randint(1,3))
+city.new_quality('structural features', 'architecture', random.randint(2,5))
+city.new_quality('design patterns', 'architecture', random.randint(1,2))
 
 city.new_quality('color schemes', 'aesthetic', random.randint(1,2))
 city.new_quality('dominant colors', 'aesthetic', random.randint(3,6))
+city.new_quality('visual motifs', 'aesthetic', random.randint(3,5))
 
 print(city)
